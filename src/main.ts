47c0d95020bf84309e2938f22b064708addbaeab
Jeannisca <njeannisca@gmail.com>
2026-06-09 18:28:07 +0200

    mise à jour

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import '../src/assets/css/index.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-Wello World
+<header>
+  <nav>
+  <ul>
+    <li><a href="/ Accueil">Accueil</a></li>
+    <li><a href="/about">About</a></li>
+    <li><a href="/contact">Contact</a></li>
+  </ul>
+  </nav>
+</header>
 `
