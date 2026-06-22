11122615c117443d20236423e1498d7bdbbc97d9
stdorsel <stergedorsel6@gmail.com>
2026-06-22 21:39:42 +0100

    Refonte du design du portail

--- src/main.ts
+++ src/main.ts
@@ -19,26 +19,9 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
       <p class="domain">Back-end</p>
       <p class="domain">Full-stack</p>
     </div>
-  </main>
-  <footer class="footer">
-    <div class="footer__top">
-      <div class="footer__top__left">
-        <img class="logo" src="./src/assets/img/custom-logo.svg" alt="DevConnect Logo">
-        <p class="slogan">Votre plateforme de référence</p>
-      </div>
-      <div class="footer__top__right">
-        <div class="location">
-          <img class="localisation__icone" src="./src/assets/img/map-pin-2-fill.svg" alt="localisation">
-          <p class="region">Libreville, Gabon</p>
-        </div>
-        <p class="call_number">num: +241 xx xx xx xx</p>
-        <p class="call_number">num: +241 xx xx xx xx</p>
-      </div>
-    </div>
-    <hr>
-    <div class="footer__bottom">
-      <p class="copyright">© 2024 DevConnect · Tous droits réservés.</p>
-      <img class="social_media" src="./src/assets/img/github-fill.svg" alt="GitHub">
-    </div>
+    <footer class="footer">
+      <p class="copyright">© 2026 DevConnect · Tous droits réservés.</p>
   </footer>
+  </main>
+  
 `;
