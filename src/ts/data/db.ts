2df2d7abee7f3b1bbb067e80e4a0119716d572e8
JordanCode <christjordan53@gmail.com>
2026-06-22 18:54:50 +0100

    ajout base de donnée TypeScript

---
+++ src/ts/data/db.ts
@@ -0,0 +1,75 @@
+export type Developpeur = {
+  nom: string;
+  specialite: string;
+  outils: string[];
+  image: string;
+  lienPortfolio: string;
+};
+
+
+
+export const developpeurs: Developpeur[]= [
+    {
+        nom: "Sterge Dorsel MBADINGA MBADINGA",
+        specialite: "Developpeur Front-end",
+        outils: [],
+        image: "../assets/img/sterge.png",
+        lienPortfolio: "https://tonton-dxy.github.io/TontonDXY.github.io/",
+    },
+    {
+        nom: "",
+        specialite: "Developpeur Front-end",
+        outils: [],
+        image: "../assets/img/",
+        lienPortfolio: "",
+    },
+    {
+        nom: "",
+        specialite: "Developpeur Front-end",
+        outils: [],
+        image: "../assets/img/",
+        lienPortfolio: "",
+    },
+    {
+        nom: "",
+        specialite: "Developpeur Front-end",
+        outils: [],
+        image: "../assets/img/",
+        lienPortfolio: "",
+    },
+    {
+        nom: "",
+        specialite: "Developpeur Front-end",
+        outils: [],
+        image: "../assets/img/",
+        lienPortfolio: "",
+    },
+    {
+        nom: "",
+        specialite: "Developpeur Front-end",
+        outils: [],
+        image: "../assets/img/",
+        lienPortfolio: "",
+    },
+    {
+        nom: "",
+        specialite: "Developpeur Front-end",
+        outils: [],
+        image: "../assets/img/",
+        lienPortfolio: "",
+    },
+    {
+        nom: "",
+        specialite: "Developpeur Front-end",
+        outils: [],
+        image: "../assets/img/",
+        lienPortfolio: "",
+    },
+    {
+        nom: "",
+        specialite: "Developpeur Front-end",
+        outils: [],
+        image: "../assets/img/",
+        lienPortfolio: "",
+    },
+]
